cb93bb60e793da11e8b8f24405d63dd2e00b6ae2
vaishakhanil <vaishakh.anil.360@gmail.com>
2025-06-04 00:32:53 -0400

    Auto-Updater Working

--- src/renderer/src/components/pages/documentation/IntroPage.jsx
+++ src/renderer/src/components/pages/documentation/IntroPage.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from 'react-router-dom'
+import {Button} from '../../atoms'
+
 export const IntroDocsPage = () => {
+    const navigate = useNavigate()
     return (
         <div>
             <h1>This is Documentation Test</h1>
+            <Button varient={"primary"} onClick={() => navigate('/')}>Back</Button>
         </div>
     )
 }
